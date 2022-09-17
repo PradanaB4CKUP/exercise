bc31c72a8f635a0f9fd57eca91f42a57ea6aea61
PradanaB4CKUP <aji2207ok@gmail.com>
2022-09-17 22:09:51 +0700

    belom selesai

---
+++ index.js
@@ -0,0 +1,61 @@
+// ! JANGAN DIMODIFIKASI
+const promiseTheaterIXX = async () => {
+    await new Promise((r) => setTimeout(r, 200));
+  
+    return new Promise((resolve) => {
+      const hasilSetelahMenonton = [
+        {
+          namaFilm: 'World of the married',
+          hasil: 'marah',
+        },
+        {
+          namaFilm: 'Inside Out',
+          hasil: 'tidak marah',
+        },
+        {
+          namaFilm: 'Nanti Kita Cerita Tentang Hari Ini',
+          hasil: 'marah',
+        },
+      ];
+  
+      return resolve(hasilSetelahMenonton);
+    });
+  };
+  
+  const promiseTheaterVGC = async () => {
+    await new Promise((r) => setTimeout(r, 200));
+  
+    return new Promise((resolve) => {
+      const hasilSetelahMenonton = [
+        {
+          namaFilm: 'KKN di Desa Penari',
+          hasil: 'marah',
+        },
+        {
+          namaFilm: 'Kukira Kau Rumah',
+          hasil: 'tidak marah',
+        },
+        {
+          namaFile: 'The Other Side',
+          hasil: 'marah',
+        },
+      ];
+  
+      return resolve(hasilSetelahMenonton);
+    });
+  };
+  
+  const promiseOutput = async () => {
+    promiseTheaterIXX()
+    .then(result => {
+        
+        return promiseTheaterIXX(result)
+    })
+    .then(result2 => {
+        return promiseTheaterVGC(result2)
+    })
+    console.log(await promiseOutput("tidak marah")); // Output: 2
+    console.log(await promiseOutput("marah")); // Output: 4
+    
+
+  };
